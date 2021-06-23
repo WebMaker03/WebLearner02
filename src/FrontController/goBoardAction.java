3d2c43b04c834c30e4ea0a5c5a9275013827ccb5
SeungJooKim <seoungjoo01@gmail.com>
2021-06-23 15:54:40 +0900

    edit controller

--- src/FrontController/goBoardAction.java
+++ src/FrontController/goBoardAction.java
@@ -1,11 +1,16 @@
 package FrontController;
 
+import java.util.ArrayList;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import DAO.BoardDAO;
 import DAO.ChallengesDAO;
 import DTO.Challenges;
+import DTO.Post;
+import DTO.Users;
 
 public class goBoardAction implements Action{
 
@@ -19,9 +24,13 @@ public class goBoardAction implements Action{
         
         BoardDAO bdao =new BoardDAO(); // boardDao에서 해당유저가 해당 챌린지에 작성한 게시글 
         // u_code 랑  c_code 넘겨 줘야ㅏㅏㅏㅏ
+      /*  HttpSession session =request.getSession();
+        Users user = (Users)session.getAttribute("session_user");*/
+        ArrayList<Post> post = bdao.c_b (c_code);
+        request.setAttribute("post", post);
         
         
-        forward.setRedirect(true); // 저장하는값이 있으면 false, 없으면 true 
+        forward.setRedirect(false); // 저장하는값이 있으면 false, 없으면 true 
         forward.setPath("Board_Chall.jsp");
         return forward;
 	}
