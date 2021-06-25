19cdcf62e35e99bfb997626401a5fec23dabba7d
현석호 <@DESKTOP-6J9KU2I>
2021-06-25 17:04:47 +0900

    .startch

--- src/FrontController/StartChallengeAction.java
+++ src/FrontController/StartChallengeAction.java
@@ -8,8 +8,10 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import DAO.ChallengesDAO;
+import DAO.UserDAO;
 import DTO.Challenges;
 import DTO.MyC;
+import DTO.Users;
 
 public class StartChallengeAction implements Action {
 
@@ -18,6 +20,8 @@ public class StartChallengeAction implements Action {
 		HttpSession session = request.getSession();
 		PrintWriter out = response.getWriter();
 		ActionForward forward =null;
+		UserDAO udao = new UserDAO();
+		Users user = (Users)session.getAttribute("session_user");
 		if (session.getAttribute("session_user") == null) {
 			response.setContentType("text/html; charset=UTF-8");
 			response.setCharacterEncoding("UTF-8");
@@ -27,27 +31,35 @@ public class StartChallengeAction implements Action {
 		} else {
 			forward = null;
 			ChallengesDAO cdao = new ChallengesDAO();
-			int u_code = Integer.parseInt(request.getParameter("u_code"));
-			ArrayList<MyC> checkMyC = cdao.prochal(u_code);
+			int u_code = user.getU_code();
+			int u_point = user.getPoint();
 			int c_code = Integer.parseInt(request.getParameter("c_code"));
 			Challenges ch = cdao.getonechal(c_code);
-			if(cdao.checkChallengeRepeat(u_code,c_code)) {
-				response.setCharacterEncoding("UTF-8");
-				response.setContentType("text/html; charset=UTF-8");
-				out.println("<script>alert('이미 참가중인 챌린지 입니다.');location.href='calltheme.ch?theme="+ ch.getTheme()+"'</script>");
-				out.flush();
-			
-			}else {
-				
-				int chFee = ch.getFee();
-				cdao.start_ch(ch, u_code);
-				cdao.updateUserPoint(chFee, u_code);
-				response.setCharacterEncoding("UTF-8");
-				response.setContentType("text/html; charset=UTF-8");
-				out.println("<script>alert('참가 신청 완료되었습니다.');location.href='calltheme.ch?theme="+ ch.getTheme()+"'</script>");
-				out.flush();
-				
-			}
+				if(ch.getFee()<u_point) {
+					if(cdao.checkChallengeRepeat(u_code,c_code)) {
+						response.setCharacterEncoding("UTF-8");
+						response.setContentType("text/html; charset=UTF-8");
+						out.println("<script>alert('이미 참가중인 챌린지 입니다.');location.href='calltheme.ch?theme="+ ch.getTheme()+"'</script>");
+						out.flush();
+					}else {
+						int chFee = ch.getFee();
+						cdao.updateUserPoint(chFee, u_code);
+						cdao.start_ch(ch, u_code);
+						Users user2 = udao.showUser(user.getUserid());
+						session.setAttribute("session_user", user2);
+
+						response.setCharacterEncoding("UTF-8");
+						response.setContentType("text/html; charset=UTF-8");
+						out.println("<script>alert('참가 신청 완료되었습니다.');location.href='calltheme.ch?theme="+ ch.getTheme()+"'</script>");
+						out.flush();
+					}
+					
+				}else {
+					response.setCharacterEncoding("UTF-8");
+					response.setContentType("text/html; charset=UTF-8");
+					out.println("<script>alert('point가 부족합니다!');location.href='calltheme.ch?theme="+ ch.getTheme()+"'</script>");
+					out.flush();
+				}
 		}
 		return forward;
 	}
